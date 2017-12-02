38da4ea450c360bac0909df138af6f6ca7a1617a
gin-lsl <gin_lsl@outlook.com>
2017-12-02 14:36:58 +0800

    init

---
+++ src/app/sign/sign.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SignRoutingModule } from './sign-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    SignRoutingModule
+  ],
+  declarations: []
+})
+export class SignModule { }
